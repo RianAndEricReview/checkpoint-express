029f8fda9212f80a3a8847e5019b7ba4bcc5d562
Eric Garnett <egcodes@gmail.com>
2018-03-13 14:06:43 -0400

    three specs pass

--- models/todos.js
+++ models/todos.js
@@ -9,9 +9,21 @@ module.exports = {
   // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
   listPeople: function () {
     // returns an array of all people for whom tasks exist
+    return Object.keys(tasks)
   },
   add: function (name, task) {
     // saves a task for a given person
+    let taskToAdd = task.complete ? task : task.complete = false
+    console.log(taskToAdd)
+    if (tasks[name]) {
+      tasks[name].push(taskToAdd)
+      // tasks[name][tasks[name].length - 1].complete = false
+    } else {
+      tasks[name] = [taskToAdd]
+      // tasks[name][tasks[name].length - 1].complete = false
+    }
+  },
+  list: function(name) {
+    return tasks[name]
   }
-  // etc.
 };
--- test/model.js
+++ test/model.js
@@ -11,16 +11,16 @@ describe('Todo model', function() {
   });
 
   describe('`listPeople` and `add`', function() {
-    xit('initially returns an empty array', function() {
+    it('initially returns an empty array', function() {
       expect(Todos.listPeople()).to.eql([]);
     });
 
-    xit('lists people after they have todos added', function() {
+    it('lists people after they have todos added', function() {
       Todos.add('zeke', { content: 'clean room' });
       expect(Todos.listPeople()).to.eql(['zeke']);
     });
 
-    xit('handles multiple people with multiple todos', function(){
+    it('handles multiple people with multiple todos', function(){
       Todos.add('zeke', { content: 'clean room' });
       Todos.add('zeke', { content: 'write mom' });
       expect(Todos.listPeople()).to.eql(['zeke']);
@@ -30,7 +30,7 @@ describe('Todo model', function() {
   });
 
   describe('`add` and `list`', function() {
-    xit('remembers who does what', function() {
+    it('remembers who does what', function() {
       Todos.add('zeke', { content: 'clean bath room' });
       expect(Todos.list('zeke')).to.have.length(1);
       Todos.add('omri', { content: 'clean living room' });
@@ -41,7 +41,7 @@ describe('Todo model', function() {
   });
 
   describe('`complete`', function() {
-    xit('is a boolean set to `false` for any new tasks', function() {
+    it('is a boolean set to `false` for any new tasks', function() {
       Todos.add('zeke', { content: 'clean self' });
       expect(Todos.list('zeke')[0].complete).to.be.false;
     });
